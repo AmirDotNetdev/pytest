9fd66f515b88294ca160dc3e6eb657569a637bae
amirdotnetdev <amirkamyabi4@gmail.com>
2023-11-30 07:42:17 +0330

    git init

---
+++ test_wallet.py
@@ -0,0 +1,28 @@
+import  pytest
+from wallet import Wallet, InsufficientAmount
+
+@pytest.fixture
+def empty_wallet():
+    return Wallet()
+
+@pytest.fixture
+def wallet():
+    return Wallet(20)
+
+def test_default_initial_amount(empty_wallet):
+    assert empty_wallet.balance == 0
+
+def test_setting_initial_amount(wallet):
+    assert wallet.balance == 20
+
+def test_wallet_add_cash(wallet):
+    wallet.add_cash(80)
+    assert wallet.balance == 100
+
+def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
+    with pytest.raises(InsufficientAmount):
+        empty_wallet.spend_cash(100)
+
+
+
+# Amir & saeed
